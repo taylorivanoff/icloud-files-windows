29a9471e5be401572aec03ab04209e3395d820c4
Taylor Ivanoff <taylorivanoff@gmail.com>
2026-01-27 21:01:48 +0100

    Remove quick actions from tray menu and jump list

--- main.js
+++ main.js
@@ -134,8 +134,6 @@ function createTray() {
   tray.setContextMenu(Menu.buildFromTemplate([
     { label: `Show ${APP_NAME}`, click: () => { if (!mainWindow) createWindow(); mainWindow.show(); } },
     { type: 'separator' },
-    { label: 'Open in Browser', click: () => shell.openExternal(ICLOUD_URL) },
-    { type: 'separator' },
     { label: 'Check for Updates', click: () => autoUpdater.checkForUpdatesAndNotify() },
     { label: 'Quit', click: () => { isQuitting = true; app.quit(); } }
   ]));
@@ -143,11 +141,7 @@ function createTray() {
 }
 
 function setupJumpList() {
-  app.setJumpList([
-    { type: 'tasks', items: [
-      { type: 'task', title: 'Open iCloud Drive', description: 'Open iCloud Drive', program: process.execPath, args: `${PROTOCOL}://open`, iconPath: process.execPath, iconIndex: 0 }
-    ]}
-  ]);
+  app.setJumpList([]);
 }
 
 function setupAutoUpdater() {
