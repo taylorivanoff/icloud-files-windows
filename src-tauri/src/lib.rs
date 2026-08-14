b2ecfb6ca48b5992406c13c6f4e0a0a50ff19421
Taylor Ivanoff <taylorivanoff@gmail.com>
2026-08-14 23:51:05 +0200

    Update iCloud Drive configuration to enable toolbar removal.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ pub fn run() {
         tauri_icloud_base::IcloudAppConfig {
             app_name: "iCloud Drive",
             url: "https://www.icloud.com/iclouddrive",
-            remove_toolbar: false,
+            remove_toolbar: true,
         },
         tauri::generate_context!(),
     );
